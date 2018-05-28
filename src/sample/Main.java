c6aca53a0edf9c8a75c91ace7dade518852b99cb
Krzysiek <krzysiek.dziedzic@outlook.com>
2018-05-28 17:06:46 +0200

    create gui

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,13 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Self-monitoring System");
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
